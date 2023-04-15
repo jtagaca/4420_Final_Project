797c18a4dc9da5c54036c8f75fbcbeb706d9fde9
James Tagaca <jtagaca@csub.edu>
2023-04-15 14:28:36 -0700

    save

--- src/Pages/InformationPage.js
+++ src/Pages/InformationPage.js
@@ -1,5 +1,11 @@
 import React from "react";
 
 export default function InformationPage() {
-  return <div>InformationPage</div>;
+  return (
+    <div className="flex items-center justify-center min-h-screen text-white bg-primary">
+      <div className="w-full p-4 m-2 bg-blue-500 border-2 border-white rounded-lg opacity-90 lg:max-w-[1145px] lg:bg-red-500">
+        hey there
+      </div>
+    </div>
+  );
 }
