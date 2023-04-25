d06514a5fba47513a5ac7fc3ee37991bad35ea90
James Tagaca <jtagaca@csub.edu>
2023-04-24 23:03:12 -0700

    added support for mobile responsiveness by adding a hamburger option

--- frontend/src/Components/NavigationBar.js
+++ frontend/src/Components/NavigationBar.js
@@ -1,10 +1,10 @@
 import logo from "../Library/images/logo.png";
 import { NavLink } from "react-router-dom";
-
+import React, { useState } from "react";
 function NavigationBar() {
-  const activeStyle = {
-    color: "white",
-    backgroundColor: "rgba(30, 58, 138, var(--tw-bg-opacity))",
+  const [menuOpen, setMenuOpen] = useState(false);
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
   };
   return (
     <nav class="border-gray bg-primary-600 opacity-75">
@@ -16,9 +16,9 @@ function NavigationBar() {
           </span>
         </a>
         <button
-          data-collapse-toggle="navbar-solid-bg"
+          onClick={toggleMenu}
           type="button"
-          class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
           aria-controls="navbar-solid-bg"
           aria-expanded="false"
         >
@@ -37,15 +37,20 @@ function NavigationBar() {
             ></path>
           </svg>
         </button>
-        <div class="hidden w-full md:block md:w-auto" id="navbar-solid-bg">
-          <ul class="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
+        <div
+          className={`${
+            menuOpen ? "block" : "hidden"
+          } w-full md:block md:w-auto`}
+          id="navbar-solid-bg"
+        >
+          <ul class="flex flex-col md:justify-center md:items-center font-medium mt-4 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
             <li>
               <NavLink
                 to="/"
                 className={({ isActive }) =>
                   isActive
-                    ? "text-blue-500 bg-blue-700 md:bg-transparent md:text-blue-700 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent"
-                    : "block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                    ? "block py-2 pl-3 pr-4 text-white bg-blue-700 rounded dark:bg-blue-600"
+                    : "block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent self-center"
                 }
               >
                 Home
@@ -56,7 +61,7 @@ function NavigationBar() {
                 to="/checkin1"
                 className={({ isActive }) =>
                   isActive
-                    ? "text-blue-500 bg-blue-700 md:bg-transparent md:text-blue-700 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent"
+                    ? "block py-2 pl-3 pr-4 text-white bg-blue-700 rounded dark:bg-blue-600"
                     : "block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                 }
               >
